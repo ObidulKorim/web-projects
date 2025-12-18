ba52368e23cd65567ea5ee8b6aa220b0d57bb7d7
saifullahkabir <saifullahkabirranaa@gmail.com>
2025-12-18 15:53:48 +0600

    initial commit

---
+++ scripts/triangle.js
@@ -0,0 +1,22 @@
+function calculateTriangleArea(){
+    // base 
+    
+   const triangleBaseInput = document.getElementById("traingle-base")
+   const triangleBaseText = triangleBaseInput.value; 
+   const base = parseFloat(triangleBaseText)   
+//    console.log(base);
+   
+//    height 
+
+const triangleHeightInput = document.getElementById('triangle-height');
+const triangleHeightText = triangleHeightInput.value;
+const height = parseFloat(triangleHeightText);
+// console.log(height);
+
+// area calculation 
+
+const area = 0.50 * base * height;
+console.log(area);
+
+
+}
